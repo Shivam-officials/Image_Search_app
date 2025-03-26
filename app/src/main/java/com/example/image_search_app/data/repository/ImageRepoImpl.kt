c060e8852bdbbecd0dd4783646eff81c582819f2
Shivam <suryabhardwajofficial@gmail.com>
2025-03-26 12:53:10 +0530

    added the imageRepoImpl

---
+++ app/src/main/java/com/example/image_search_app/data/repository/ImageRepoImpl.kt
@@ -0,0 +1,35 @@
+package com.example.image_search_app.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.example.image_search_app.data.Mappers.ImageDTOtoImageMapper
+import com.example.image_search_app.data.pagingSource.ImagePagingSource
+import com.example.image_search_app.data.remote.ApiService
+import com.example.image_search_app.domain.model.Image
+import com.example.image_search_app.domain.repository.ImageRepository
+import javax.inject.Inject
+
+
+class ImageRepoImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val mapper: ImageDTOtoImageMapper
+) : ImageRepository {
+    override fun getImages(q: String): Pager<Int, Image> {
+        return Pager(
+            config = PagingConfig(
+                initialLoadSize = 10,
+                prefetchDistance = 1,
+                pageSize = 10,
+                enablePlaceholders = true,
+            ),
+            pagingSourceFactory = {
+                ImagePagingSource(
+                    apiService = apiService,
+                    mapper = mapper,
+                    q = q
+                )
+            }
+
+            )
+    }
+}
